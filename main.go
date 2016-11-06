a8de9f4be138fd4223822e6db0a6047d0ef1b84a
Vlad Golubev <strikepark@ya.ru>
2016-11-06 09:03:42 +0300

    add PostgreSQL connection

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	_ "github.com/lib/pq"
 
 	"os"
 	"strconv"
 
 	_ "planadotest/routers"
+	"database/sql"
+	"log"
 )
 
 func main() {
+	_, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	port, err := strconv.Atoi(os.Getenv("PORT"))
 
 	if err == nil {

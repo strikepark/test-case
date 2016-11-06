0383a0f284c89dff76d7fc9d269f625ba6dd85cb
Vlad Golubev <strikepark@ya.ru>
2016-11-06 13:23:33 +0300

    fix database init

--- main.go
+++ main.go
@@ -11,11 +11,23 @@ import (
 	//"log"
 
 	_ "planadotest/routers"
+	"fmt"
 )
 
 func init() {
 	orm.RegisterDriver("postgres", orm.DRPostgres)
 	orm.RegisterDataBase("default", "postgres", os.Getenv("DATABASE_URL"))
+
+	o := orm.NewOrm()
+
+	res, err := o.Raw("CREATE TABLE IF NOT EXISTS " +
+		`orders("Id" integer, "Code" bigint, ` +
+		`"SendAddress" varchar(255), "RecipientAddress" varchar(255), ` +
+		`"PhoneNumber" bigint, "Status" varchar(255))`).Exec()
+	if err == nil {
+		num, _ := res.RowsAffected()
+		fmt.Println("postgres row affected nums: ", num)
+	}
 }
 
 func main() {

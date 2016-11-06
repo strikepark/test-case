b44da6e6455c1982e231e3a60c7eaff63729d345
Vlad Golubev <strikepark@ya.ru>
2016-11-06 19:28:19 +0300

    update

--- routers/router.go
+++ routers/router.go
@@ -2,18 +2,21 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
-	"planadotest/controllers"
+	c "planadotest/controllers"
 )
 
 func init() {
-	ns := beego.NewNamespace("/api",
-		beego.NSRouter("/orders", &controllers.OrderController{}, "post:CreateOrder"),
-		beego.NSRouter("/orders", &controllers.OrderController{}, "get:GetOrders"),
+	//ns := beego.NewNamespace("/api",
+		//beego.NSRouter("/orders", &controllers.OrderController{}, "post:CreateOrder"),
+		//beego.NSRouter("/orders", &controllers.OrderController{}, "get:GetOrders"),
 		//beego.NSRouter("/orders/:id", &controllers.OrderController{}, "get:GetOrder"),
 		//beego.NSRouter("/orders/:id", &controllers.OrderController{}, "put:UpdateOrder"),
 		//
 		//beego.NSRouter("/orders/costumer/:code", &controllers.OrderController{}, "get:GetCostumerOrders"),
-	)
+	//)
 
-	beego.AddNamespace(ns)
+	//beego.AddNamespace(ns)
+
+	beego.Router("/api/orders", &c.OrderController{}, "post:CreateOrder")
+	beego.Router("/api/orders", &c.OrderController{}, "get:GetOrders")
 }

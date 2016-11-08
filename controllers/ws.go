823e4fefb99267b31aff0de81e7ee03e1764a892
Vlad Golubev <strikepark@ya.ru>
2016-11-08 18:16:36 +0300

    update websocket

--- controllers/ws.go
+++ controllers/ws.go
@@ -4,7 +4,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/gorilla/websocket"
 
-	"net/http"
+	//"net/http"
 	"fmt"
 )
 
@@ -15,8 +15,8 @@ type WsController struct {
 func (this *WsController) WsHandle() {
 	ws, err := websocket.Upgrade(this.Ctx.ResponseWriter, this.Ctx.Request, nil, 1024, 1024)
 	if _, ok := err.(websocket.HandshakeError); ok {
-		//fmt.Println(this.Ctx.ResponseWriter, "Not a websocket handshake", 400)
-		http.Error(this.Ctx.ResponseWriter, "Not a websocket handshake", 400)
+		fmt.Println(this.Ctx.ResponseWriter, "Not a websocket handshake", 400)
+		//http.Error(this.Ctx.ResponseWriter, "Not a websocket handshake", 400)
 		return
 	} else if err != nil {
 		beego.Error("Cannot setup WebSocket connection:", err)

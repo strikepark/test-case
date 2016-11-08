3045d1a1918ef3198d0741998ea3dfdebf9bcf96
Vlad Golubev <strikepark@ya.ru>
2016-11-08 18:32:07 +0300

    update websocket

--- routers/router.go
+++ routers/router.go
@@ -6,6 +6,8 @@ import (
 )
 
 func init() {
+	beego.Router("/ws", &c.WsController{}, "*:WsHandle")
+
 	ns := beego.NewNamespace("/api",
 		beego.NSRouter("/orders", &c.OrderController{}, "put:CreateOrder"),
 		beego.NSRouter("/orders", &c.OrderController{}, "get:GetOrders"),
@@ -13,7 +15,7 @@ func init() {
 		beego.NSRouter("/orders/:id", &c.OrderController{}, "put:UpdateOrder"),
 
 		beego.NSRouter("/orders/costumer/:phoneNumber", &c.OrderController{}, "get:GetCostumerOrders"),
-		beego.NSRouter("/ws", &c.WsController{}, "*:WsHandle"),
+		//beego.NSRouter("/ws", &c.WsController{}, "*:WsHandle"),
 	)
 
 	beego.AddNamespace(ns)

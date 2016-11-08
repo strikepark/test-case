f8dc21d5b37f3a18868b52548655c2e97095fb65
Vlad Golubev <strikepark@ya.ru>
2016-11-08 17:41:02 +0300

    update websocket

--- controllers/ws.go
+++ controllers/ws.go
@@ -22,6 +22,8 @@ type message struct {
 }
 
 func (this *WsController) WsHandle() {
+	fmt.Println("http://" + this.Ctx.Request.Host)
+
 	if this.Ctx.Request.Header.Get("Origin") != ("http://" + this.Ctx.Request.Host) {
 		fmt.Println(this.Ctx.ResponseWriter, "Origin not allowed", 403)
 		return

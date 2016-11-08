42db909e01d2c23ba4532b50153b2fc3b8e9b4c2
Vlad Golubev <strikepark@ya.ru>
2016-11-08 09:02:59 +0300

    update websocket

--- controllers/ws.go
+++ controllers/ws.go
@@ -3,22 +3,39 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"github.com/gorilla/websocket"
+	"log"
 )
 
 type WsController struct {
 	beego.Controller
 }
 
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func (this *WsController) WsHandle() {
-	var conn, _ = upgrader.Upgrade(this.Ctx.ResponseWriter, this.Ctx.Request, nil)
+	var conn, err = upgrader.Upgrade(this.Ctx.ResponseWriter, this.Ctx.Request, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-	go func(conn *websocket.Conn) {
+	//go func(conn *websocket.Conn) {
 		for {
-			mType, msg, _ := conn.ReadMessage()
+			mType, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			if err = conn.WriteMessage(mType, msg); err != nil {
+				log.Println(err)
+				return
+			}
 
 			conn.WriteMessage(mType, msg)
 		}
-	}(conn)
+	//}(conn)
 }

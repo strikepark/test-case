0002c29c4adf7bbce447c9920b4bc899e31521ae
Vlad Golubev <strikepark@ya.ru>
2016-11-08 21:52:36 +0300

    Example with chat

--- main.go
+++ main.go
@@ -1,52 +1,5 @@
-package main
-
-import (
-	"golang.org/x/net/websocket"
-	"net/http"
-	"fmt"
-	//"strconv"
-	"os"
-	"log"
-)
-
-func echoHandler(ws *websocket.Conn) {
-
-	for {
-		receivedtext := make([]byte, 100)
-
-		n, err := ws.Read(receivedtext)
-
-		if err != nil {
-			fmt.Printf("Received: %d bytes\n",n)
-		}
-
-		s := string(receivedtext[:n])
-		fmt.Printf("Received: %d bytes: %s\n",n,s)
-	}
-}
-
-func determineListenAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return "", fmt.Errorf("$PORT not set")
-	}
-	return ":" + port, nil
-}
-
-func main() {
-	addr, err := determineListenAddress()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	http.Handle("/echo", websocket.Handler(echoHandler))
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	err = http.ListenAndServe(addr, nil)
-	if err != nil {
-		panic("Error: " + err.Error())
-	}
-}
-
+//package main
+//
 //import (
 //	"github.com/astaxie/beego"
 //	_ "github.com/lib/pq"
@@ -86,15 +39,114 @@ func main() {
 //		fmt.Println("postgres row affected nums: ", num)
 //	}
 //}
-
+//
 //func main() {
-	//port, err := strconv.Atoi(os.Getenv("PORT"))
-	//
-	//if err == nil {
-	//	beego.BConfig.Listen.HTTPPort = port
-	//}
-	//
-	//beego.SetStaticPath("/", "static_html")
-	//
-	//beego.Run()
+//	port, err := strconv.Atoi(os.Getenv("PORT"))
+//
+//	if err == nil {
+//		beego.BConfig.Listen.HTTPPort = port
+//	}
+//
+//	beego.SetStaticPath("/", "static_html")
+//
+//	beego.Run()
 //}
+
+package main
+
+import (
+	"net/http"
+	"os"
+	"fmt"
+	"encoding/json"
+	"io"
+
+	"github.com/gorilla/websocket"
+	"github.com/pkg/errors"
+)
+
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+)
+
+// message sent to us by the javascript client
+type message struct {
+	Handle string `json:"handle"`
+	Text   string `json:"text"`
+}
+
+// validateMessage so that we know it's valid JSON and contains a Handle and
+// Text
+func validateMessage(data []byte) (message, error) {
+	var msg message
+
+	if err := json.Unmarshal(data, &msg); err != nil {
+		return msg, errors.Wrap(err, "Unmarshaling message")
+	}
+
+	if msg.Handle == "" && msg.Text == "" {
+		return msg, errors.New("Message has no Handle or Text")
+	}
+
+	return msg, nil
+}
+
+// handleWebsocket connection.
+func handleWebsocket(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		m := "Unable to upgrade to websockets"
+		fmt.Println("err", err, m)
+		http.Error(w, m, http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("РАБОТАЕТ")
+
+	for {
+		mt, data, err := ws.ReadMessage()
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Websocket closed!")
+			} else {
+				fmt.Println("Error reading websocket message")
+			}
+
+			break
+		}
+
+		switch mt {
+		case websocket.TextMessage:
+			msg, err := validateMessage(data)
+			if err != nil {
+				fmt.Println("msg:", msg, "err:", err)
+				break
+			}
+			fmt.Println(string(data[:]))
+		default:
+			fmt.Println("Unknown Message!")
+		}
+	}
+
+	ws.WriteMessage(websocket.CloseMessage, []byte{})
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("PORT ERROR")
+	}
+
+	http.Handle("/", http.FileServer(http.Dir("./static_html")))
+	http.HandleFunc("/ws", handleWebsocket)
+	fmt.Println(http.ListenAndServe(":" + port, nil))
+}

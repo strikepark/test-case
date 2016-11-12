0e505901dc0e108531ffe72b61e941d86bcdf2f7
Vlad Golubev <strikepark@ya.ru>
2016-11-12 23:08:57 +0300

    fix

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -14,22 +14,17 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'js/'),
     filename: 'bundle.js',
-    publicPath: '/js/'
+    // publicPath: '/js/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    // new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new NpmInstallPlugin(),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
       compress: {
         warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
       }
     })
   ],
